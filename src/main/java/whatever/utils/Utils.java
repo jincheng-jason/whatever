74b6149cb85977db3cbf7c9e366daa898c831f8d
lijc <lijc1990@yeah.net>
2015-04-07 00:02:28 +0800

    Delete useless code

--- src/main/java/whatever/utils/Utils.java
+++ src/main/java/whatever/utils/Utils.java
@@ -28,40 +28,4 @@ public class Utils {
         return returnList;
     }
 
-
-    
-//    public static Map<String,String> returnSuccess(){
-//        Map<String, String> map = new HashMap<String, String>();
-//        map.put("oper_code","1");
-//        map.put("message","操作成功");
-//        return map;
-//    }
-//
-//    public static Map<String,String> returnFailed(){
-//        Map<String, String> map = new HashMap<String, String>();
-//        map.put("oper_code","0");
-//        map.put("message","操作失败");
-//        return map;
-//    }
-//
-//    public static Map<String,String> returnSuccess(String name){
-//        Map<String, String> map = new HashMap<String, String>();
-//        map.put("oper_code","1");
-//        map.put("message",name + "成功");
-//        return map;
-//    }
-//
-//    public static Map<String,String> returnFailed(String name){
-//        Map<String, String> map = new HashMap<String, String>();
-//        map.put("oper_code","0");
-//        map.put("message",name + "失败");
-//        return map;
-//    }
-//
-//    public static Map<String,String> voidValue(String name){
-//        Map<String, String> map = new HashMap<String, String>();
-//        map.put("oper_code","0");
-//        map.put("message",name + "缺少参数");
-//        return map;
-//    }
 }
